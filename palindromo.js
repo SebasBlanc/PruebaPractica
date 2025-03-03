cc2b4acedcff03ac9b0c81bff5dd23d658386dd4
Sebastián Blanco Bolaños <sebastianblancobolanos@gmail.com>
2025-03-03 14:57:31 -0600

    Update palindromo.js

--- palindromo.js
+++ palindromo.js
@@ -1,8 +1,8 @@
 
 function palindromo(palabra) {
-    const limpiarPalabra = palabra.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    const invertir = limpiarPalabra.split('').reverse().join('');
-    if (limpiarPalabra === invertir) {
+    const limpiarPalabra = palabra.replace(/[^A-Za-z0-9]/g, '').toLowerCase(); // Elimina los caracteres especiales y los espacios
+    const invertir = limpiarPalabra.split('').reverse().join(''); // Invierte la palabra
+    if (limpiarPalabra === invertir) { // Compara si la palabra original es igual a la palabra invertida
         console.log(true);
     } else {
         console.log(false);
